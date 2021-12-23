586148f341d43529d76ec9aff53dbb6eea1bda04
Sebastian Ionel <se3bofficial@gmail.com>
2021-12-23 13:12:10 +0200

    Uploaded the final version of the Graph

--- Graph.cpp
+++ Graph.cpp
@@ -7,14 +7,27 @@
 #include <queue>
 #include <algorithm>
 #include "Graph.h"
+#include <fstream>
 
 #pragma region constructors
-Graph::Graph(int numberOfElements) : numberOfElements(numberOfElements) {}
+Graph::Graph(int numberOfElements, int numberOfEdges, bool isOriented) : numberOfElements(numberOfElements), numberOfEdges(numberOfEdges), isOriented(isOriented) {}
+Graph::Graph() {
+    numberOfElements = 0;
+    numberOfEdges = 0;
+    isOriented = false;
+}
 
-Graph::Graph() {}
 #pragma endregion
 
 #pragma region functions that help at reading and writing
+
+int Graph::getDiameter() {
+    return diameterOfTree;
+}
+int Graph::getLastNode() {
+    return lastNode;
+}
+
 void Graph::addEdgeOriented(int a, int b) {
     if(a != b) {
         adjacentList[a].push_back(b);
@@ -22,29 +35,33 @@ void Graph::addEdgeOriented(int a, int b) {
 }
 
 void Graph::addEdgeUnoriented(int a, int b) {
-    adjacentList[a].push_back(b);
-    adjacentList[b].push_back(a);
+    if(a == b) {
+        adjacentList[a].push_back(b); // solving duplicates
+    }
+    else {
+        adjacentList[a].push_back(b);
+        adjacentList[b].push_back(a);
+    }
+
 }
 
-void Graph::readOriented() {
+void Graph::readOriented(std::ifstream &file) {
     int nodes, edges;
-    std::cin >> nodes >> edges;
+    file >> nodes >> edges;
 
     int x, y; // for reading the edges
     while(edges != 0) {
-        std::cin >> x >> y;
+        file >> x >> y;
         addEdgeOriented(x, y);
         edges--;
     }
 }
 
-void Graph::readUnoriented() {
-    int nodes, edges;
-    std::cin >> nodes >> edges;
-
+void Graph::readUnoriented(std::ifstream &file) {
+    int edges = numberOfElements - 1;
     int x, y; // for reading the edges
-    while(edges != 0) {
-        std::cin >> x >> y;
+    while (edges != 0) {
+        file >> x >> y;
         addEdgeUnoriented(x, y);
         edges--;
     }
@@ -63,6 +80,34 @@ void Graph::printGraph() {
 #pragma endregion
 
 #pragma region graph algorithms
+
+void Graph::royFloydAlgorithm(int weightMatrix[100][100]) {
+    for(int k = 0; k < numberOfElements; k++) {
+        for(int i = 0; i < numberOfElements; i++) {
+            for(int j = 0; j < numberOfElements; j++) {
+                if(
+                        i != j &&
+                        weightMatrix[i][k] &&
+                        weightMatrix[k][j] &&
+                        (weightMatrix[i][j] > weightMatrix[i][k] + weightMatrix[k][j] || !weightMatrix[i][j])
+
+                        ) {
+                    weightMatrix[i][j] = weightMatrix[i][k] + weightMatrix[k][j];
+                }
+                // if(
+                //         weightMatrix[i][k] &&
+                //         weightMatrix[k][j] &&
+                //         (weightMatrix[i][j] > weightMatrix[i][k] + weightMatrix[k][j] || !weightMatrix[k][j] && i != j) -> greseala i!=j in par.
+                //     )
+                //     {
+                //         weightMatrix[i][j] = weightMatrix[i][k] + weightMatrix[k][j];
+                //     }
+            }
+        }
+    }
+}
+
+// DFS -> ITERATIVE AND RECURSIVE
 void Graph::dfsRec(int node) {
 
     int visitedNodes[100000] = {0};
@@ -105,6 +150,56 @@ void Graph::dfsIter(int node) {
     }
 }
 
+// INFOARENA: BFS
+void Graph::bfs(int node) {
+
+    int visitedNodes[100000] = {0}, nodeDegree[100000] = {0};
+    int distance[100000] = {0};
+
+    std::queue<int> nodeQueue;
+    visitedNodes[node] = 1;
+    nodeQueue.push(node);
+
+    // Modify the BFS for InfoArena
+    // you need a counter array so that you can measure the distance
+    int counter[100000] = {0};
+
+    counter[node] = 1; // set the starting node's counter to 1
+
+
+    while(!nodeQueue.empty()) {
+        // pop the front
+        int front = nodeQueue.front();
+        // find the neighbours of the current node
+        for(auto theOtherNodes : adjacentList[front]) {
+            // theOtherNodes = nod[el][i]
+            // contor = counter;
+            if(visitedNodes[theOtherNodes] == 0) {
+                // mark them and then increase the distance between the nodes
+                nodeQueue.push(theOtherNodes);
+                visitedNodes[theOtherNodes] = 1;
+
+                // modification for InfoArena
+                counter[theOtherNodes] = counter[front] + 1; // increase the counter for the visited node.
+
+                diameterOfTree = counter[theOtherNodes];
+                lastNode = theOtherNodes;
+            }
+        }
+        nodeQueue.pop();
+    }
+    //output the distances
+    // for(int i = 0; i < numberOfElements; i++) {
+    //     if(visitedNodes[i] != 0) {
+    //         std::cout << distance[i] << " ";
+    //     }
+    //     else {
+    //         std::cout << -1 << " ";
+    //     }
+    // }
+}
+
+//INFOARENA -> Connected components
 int Graph::numberOfConnectedComponents() {
 
     int visitedNodes[100000] = {0};
@@ -121,6 +216,7 @@ int Graph::numberOfConnectedComponents() {
     return counter;
 }
 
+//INFOARENA -> Topological Sort (with BFS, tried with DFS and it doesn t work propely)
 void Graph::topologicalSort() {
     int visitedNodes[100000] = {0}, nodeDegree[100000] = {0};
 
@@ -166,61 +262,24 @@ void Graph::topologicalSort() {
     }
 }
 
-void Graph::bfs(int node) {
 
-    int visitedNodes[100000] = {0}, nodeDegree[100000] = {0};
-    int distance[100000] = {0};
+// Check the Havel Hakimi for a given array
+bool Graph::checkHavelHakimi(std::vector<int>& nodeDegrees) {
+    // used the STL vector because it's easier than working with arrays.
 
-    std::queue<int> nodeQueue;
-    visitedNodes[node] = 1;
-    nodeQueue.push(node);
-
-    while(!nodeQueue.empty()) {
-        // pop the front
-        int front = nodeQueue.front();
-        nodeQueue.pop();
-
-        // find the neighbours of the current node
-        for(auto theOtherNodes : adjacentList[front]) {
-            if(visitedNodes[theOtherNodes] == 0) {
-                // mark them and then increase the distance between the nodes
-                visitedNodes[theOtherNodes] = 1;
-                nodeQueue.push(theOtherNodes);
-                distance[theOtherNodes] = distance[front] + 1;
-            }
-        }
-    }
-    //output the distances
-    for(int i = 0; i < numberOfElements; i++) {
-        if(visitedNodes[i] != 0) {
-            std::cout << distance[i] << " ";
-        }
-        else {
-            std::cout << -1 << " ";
-        }
-    }
-}
-
-int Graph::checkHavelHakimi(std::vector<int>& nodeDegrees) {
-    // used the STL vector because it's easier than working with arrays here.
-
-
-    while(1) {
+    while(true) {
         // if the nodeDegrees vector is empty or it only contains zeroes, then it's valid
         if(nodeDegrees.empty() || std::all_of(nodeDegrees.begin(), nodeDegrees.end(), [](int i) {return i == 0;})) {
-            return 1;
+            return true;
         }
 
         // sort the vector
         std::sort(nodeDegrees.begin(), nodeDegrees.end());
 
-        for(int i : nodeDegrees) {
-            std::cout << i << " ";
-        }
         std::cout << "\n";
         int lastElement = nodeDegrees[nodeDegrees.size() - 1];
         if(lastElement > nodeDegrees.size() - 1) {
-            return 0;
+            return false;
         }
 
         nodeDegrees.erase(nodeDegrees.begin() + nodeDegrees.size() - 1);
@@ -230,12 +289,81 @@ int Graph::checkHavelHakimi(std::vector<int>& nodeDegrees) {
                 nodeDegrees[i]--;
             }
             else {
-                return 0;
+                return false;
             }
         }
     }
 }
 
+void Graph::eulerFunction(int node, std::vector<int> &cycles) {
+    // verifying the parity for every node
+    for (int i=0;i<=numberOfElements; i++) {
+        int co=0;
+        for(auto& neighbour : adjacentList[i]){
+            if(neighbour!=i)
+                co++;
+            else
+                co+=2;
+        }
+        if(co%2==1)
+            return;
+    }
+
+    std::stack<int> nodeStack;
+    // cycles.push_back(node);
+    nodeStack.push(node);
+
+    while (!nodeStack.empty()) {
+
+        int currentNode = nodeStack.top();
+        // std::vector<int> currentCycles = std::get<1>(currentTuple);
+        // nodeStack.pop();
+
+        if (!adjacentList[currentNode].empty()) {
+
+            int neighbour = adjacentList[currentNode].back();
+            adjacentList[currentNode].pop_back();
+
+         if (currentNode != neighbour) {
+
+            auto find = std::find(adjacentList[neighbour].begin(), adjacentList[neighbour].end(), currentNode);
+            if (find != adjacentList[neighbour].end()) {
+                *find = adjacentList[neighbour].back();
+                adjacentList[neighbour].pop_back();
+            }
+         }
+
+            nodeStack.push(neighbour);
+        } else {
+            nodeStack.pop();
+            cycles.push_back(currentNode);
+        }
+//        cycles.push_back(currentNode);
+    }
+}
+
+std::vector<int> Graph::getEulerianCycle() {
+    std::vector<int> result;
+    // std::stack<std::tuple<int, std::vector<int>>> nodeStack;
+    result.reserve(numberOfElements);
+
+    eulerFunction(1, result);
+    if(result.size()!=numberOfEdges+1)
+        return {};
+
+    for(int i = 0; i < numberOfElements; i++) {
+
+        if(!adjacentList[i].empty()){
+            return {};
+        }
+    }
+    for(auto& i: result) {
+        i++;
+    }
+
+    return result;
+
+}
 
 
 
--- Graph.h
+++ Graph.h
@@ -13,31 +13,46 @@ private:
 #pragma region attributes
 
     int numberOfElements; // the number of nodes from the graph
+    int numberOfEdges;
+    int diameterOfTree; // The diameter of the tree, used for InfoArena
+    int lastNode;
+    bool isOriented; // bool that checks if the graph is oriented / unoriented (oriented -> true, unoriented -> false)
+    // todo: schimba std::vector
     std::vector<int> adjacentList[100000]; // implementing the graph using an array of adjacentLists
 
+
 #pragma endregion
 
-#pragma region private functions that help at stuff
-    void addEdgeOriented(int a, int b); // if we are working with an oriented graph
-    void addEdgeUnoriented(int a, int b); // if we are working with an unoriented graph
+#pragma region private functions that help at building the graph
+    void addEdgeOriented(int a, int b); // using this function at reading if we are working with an oriented graph
+    void addEdgeUnoriented(int a, int b); // using this function at reading if we are working with an unoriented graph
+    void eulerFunction(int node, std::vector<int> &cycles);
 #pragma endregion
+
 public:
+    // todo: FUNCTII CARE RETURNEAZA!
 #pragma region public functions related to the class
 
-    Graph(int numberOfElements);
-
+    Graph(int numberOfElements, int numberOfEdges, bool isOriented);
     Graph();
 
-    void readOriented(); // reading the data for an oriented graph
+    // void checkTypeAndRead(); // check the type of the graph
+    void readOriented(std::ifstream &file); // reading the data for an oriented graph
+    void readUnoriented(std::ifstream &file); // reading the data for an unoriented graph
     void printGraph(); // output the content from the graph
-    void readUnoriented(); // reading the data for an unoriented graph
-
     void dfsRec(int node); // recursive definition of the DFS for the given node.
     void dfsIter(int node); // iterative definitions of the DFS for the given node.
+    void bfs(int node); // BFS for the given node, outputs the distances;
+
+    int getDiameter(); // getter for the diameter
+    int getLastNode(); // getter for the last node after the bfs
+
     int numberOfConnectedComponents(); // returns the number of connected components in the Graph
     void topologicalSort(); // topological sort using Kahn's Algorithm
-    void bfs(int node); // BFS for the given node, outputs the distances;
-    int checkHavelHakimi(std::vector<int>& nodeDegrees); // returns the value of the Havel Hakimi algorithm
+    bool checkHavelHakimi(std::vector<int>& nodeDegrees); // returns the value of the Havel Hakimi algorithm
+    std::vector<int> getEulerianCycle();
+
+    void royFloydAlgorithm(int weightMatrix[100][100]); // for solving the royfloyd algorithm
 
 #pragma endregion
 
